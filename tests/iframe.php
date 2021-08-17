c712c1326f40feb0c10bb18d01d2bf78a202e0f3
purencool <purencool@gmail.com>
2021-08-18 09:06:52 +1000

    Updated iframe test

--- tests/iframe.php
+++ tests/iframe.php
@@ -25,12 +25,21 @@ echo '
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
-    <link rel="stylesheet" href="./assets/style.css">
+    <title>Iframe test</title>
+    <style>
+     body{
+      background:white;
+     }
+    </style>
   </head>
   <body>
-    <h1>Testing to see</h1>
-    <script src="./assets/iframe.js"></script>
+    <h1>Test Iframe</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+    <p>This is some text in a paragraph.</p>
   </body>
 </html>';
 
