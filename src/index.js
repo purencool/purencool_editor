71bb7f3c7f6ad5f13a7586ccbdb2914828227c41
purencool <purencool@gmail.com>
2021-08-21 16:23:02 +1000

    Updated CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Editor from "./Components/Editor";
 import About from "./Components/Settings";
@@ -9,6 +9,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
+        <Route exact path="/">
+           <Redirect to="/purencool-editor" />
+        </Route>
         <Route path="/purencool-editor">
           <Editor />
         </Route>
