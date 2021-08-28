9ab40f12ef56923a5f2a1c49cdd9f8a1fd2c95be
purencool <purencool@gmail.com>
2021-08-28 16:23:36 +1000

    Refactored code

--- src/Components/Editor/Parts/DynamicEditor.js
+++ src/Components/Editor/Parts/DynamicEditor.js
@@ -16,43 +16,12 @@ import jQuery from "jquery";
  */
 const DynamicEditor = () => {
 
+  /**
+   * 
+   * @type type
+   */
   const [globalVars] = useGlobalState("global_vars");
   
-  
-  /**
-   * 
-   * @returns {undefined}
-   */
-  const live = async () => {
-    console.log(inputList);
-    console.log(globalVars.api_url);
-    if (globalVars.api_url !== "undefined") {
-      await axios
-              .post(globalVars.api_url, inputList, {})
-              .then((res) => console.log(res.statusText))
-              .catch((err) => console.log("Error", err));
-    }
-  };
-  
-  
-  /**
-   * 
-   * @returns {undefined}
-   */
-  const compile = async () => {
-    console.log(inputList);
-    console.log(globalVars.compile_api_url);
-    if (globalVars.compile_api_url !== "undefined") {
-         const res = await axios
-                .post(globalVars.compile_api_url, {"compile": inputList}, {})
-                //.then(response => {
-                //console.log(response.data.live_response);
-                // }) 
-                .catch((err) => console.log("Error", err));
-        console.log("JSON data from API ==>", res.data.compile_response);
-    }
-  };
-
   /**
    * 
    * @type type
@@ -72,6 +41,37 @@ const DynamicEditor = () => {
     setInputList(list);
   };
 
+  
+  
+  function complileLiveAccess(scssUpdate) {
+      let results = false;
+
+      if (globalVars.scss_api_url !== "undefined") {
+        let braces = (scssUpdate.split("{").length - 1) + (scssUpdate.split("}").length - 1);
+        let colons = (scssUpdate.split(":").length - 1) + (scssUpdate.split(";").length - 1);
+        
+        if (braces%2 === 0 & colons%2 === 0 & (braces+colons) !== 0){
+         if(window.purencool_editor_config.globalKeyPress === "1") {
+            window.purencool_editor_config["globalKeyPress"] = "0";
+	        results = true;
+          }
+        } 
+      }
+
+      return results;
+   }
+  
+  
+  
+  /**
+   * 
+   * @returns {undefined}
+   */
+  const live = async () => {
+
+  };
+  
+  
   /**
    * 
    * @param {type} code
@@ -95,25 +95,7 @@ const DynamicEditor = () => {
       scssUpdate = scssUpdate + inputList[i].code;
     }
 
-    function complileLiveAccess(scssUpdate) {
-      let results = false;
 
-      if (globalVars.scss_api_url !== "undefined") {
-        let braces = (scssUpdate.split("{").length - 1) + (scssUpdate.split("}").length - 1);
-        let colons = (scssUpdate.split(":").length - 1) + (scssUpdate.split(";").length - 1);
-        
-        if (braces%2 === 0 & colons%2 === 0 & (braces+colons) !== 0){
-         if(window.purencool_editor_config.globalKeyPress === "1") {
-            window.purencool_editor_config["globalKeyPress"] = "0";
-            console.log('Numbers is so going to the compiler'); 
-	        results = true;
-          }
-        } 
-
-      }
-
-      return results;
-    }
 
     
     if (complileLiveAccess(scssUpdate) === true) {
@@ -142,6 +124,27 @@ const DynamicEditor = () => {
     }
   };
 
+  
+  
+  /**
+   * 
+   * @returns {undefined}
+   */
+  const compile = async () => {
+    console.log(inputList);
+    console.log(globalVars.compile_api_url);
+    if (globalVars.compile_api_url !== "undefined") {
+       const res = await axios
+          .post(globalVars.compile_api_url, {"compile": inputList}, {})
+          //.then(response => {
+          //console.log(response.data.live_response);
+          // }) 
+          .catch((err) => console.log("Error", err));
+    }
+  };
+
+
+  
   /**
    * 
    * @param {type} index
