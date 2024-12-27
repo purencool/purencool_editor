dc34464f5b71a747ff9d2c07e860d71a384c2932
Purencool <purencool@gmail.com>
2024-12-28 08:11:55 +1100

    Upgrade to all packages

--- src/App/Editor/Components/DynamicEditor/Parts/CssFiles.jsx
+++ src/App/Editor/Components/DynamicEditor/Parts/CssFiles.jsx
@@ -3,31 +3,31 @@ import store from "../../../Components/Util/store";
 
 /**
  * Returns CssFiles information.
- * 
+ *
  * @param props
  *   Object props.
- * @returns object Help 
+ * @returns object Help
  *   Response object before rendering.
  */
 const CssFiles = (props) => {
-  
+
   let ideNumber = props.ideNumber;
 
 
   /**
    * InputList saves all the data collected in the Editors.
-   *  
-   * The inputList function contains an array of objects that is changed by 
-   * setRatio and updates useState. 
-   * 
-   * @type array 
+   *
+   * The inputList function contains an array of objects that is changed by
+   * setRatio and updates useState.
+   *
+   * @type array
    *   Returns array of Json objects.
    */
   const [inputList, setInputList] = store.useState("global_editor_array");
 
   /**
    * Global Vars.
-   * 
+   *
    * @type object global_vars.
    *   Returns global_vars set at the start of the application.
    */
@@ -35,10 +35,10 @@ const CssFiles = (props) => {
 
   /**
    * Receives data from the select input.
-   * 
+   *
    * This function collates the data from the select input
    * and adds it to the inputList to be used later on in different contexts.
-   *  
+   *
    * @param object inputValue
    *   Data object from dropdown input.
    * @param int index
@@ -58,29 +58,31 @@ const CssFiles = (props) => {
       }
       setInputList(parseList);
     };
-  
+
 
   /**
    * Options creation and store results.
-   * 
+   *
    * @type object options.
    *   Returns options array creation.
-   */ 
+   */
   const options = [];
     for (const [key, value] of Object.entries(globalVars[0].css_files)) {
       if(inputList[ideNumber]['configuration']['css_files'] == undefined){
         handleCssFileChange('default', ideNumber)
       }
-      if(key == inputList[ideNumber]['configuration']['css_files']['value']){
-        options.push(<option value={key} key={key} selected >{value.name}</option>);
-      } else{
-        options.push(<option value={key} key={key}>{value.name}</option>);
+      if (inputList[ideNumber]['configuration']['css_files']!= undefined) {
+        if (key == inputList[ideNumber]['configuration']['css_files']['value']) {
+          options.push(<option value={key} key={key}>{value.name}</option>);
+        } else {
+          options.push(<option value={key} key={key}>{value.name}</option>);
+        }
       }
     }
 
   return (
-      <select name={`code-editor-css-file-${ideNumber}`} onChange={e => handleCssFileChange(e.target.value, ideNumber)} >  
-        { options }          
+      <select name={`code-editor-css-file-${ideNumber}`} onChange={e => handleCssFileChange(e.target.value, ideNumber)} >
+        { options }
       </select>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    emptyOutDir: false,
+    sourcemap: process.env.MODE !== 'production',
+    outDir: "dist",
+    manifest: true,
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        app: path.resolve(__dirname, './src/main.jsx'),
+      },
+    },
+  }
 })
+
